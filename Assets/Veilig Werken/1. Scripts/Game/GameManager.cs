a64274b7b0bdb9c7ac38dfc0518ee5c0434c4039
Mathias Bevers <bevertje01@outlook.com>
2021-05-12 15:41:53 +0200

    Added player Movement & animation
    Added a simple 2d physics based player movement class and it's corresponding animations.

--- Assets/Veilig Werken/1. Scripts/Game/GameManager.cs
+++ Assets/Veilig Werken/1. Scripts/Game/GameManager.cs
@@ -2,12 +2,8 @@
 
 namespace VeiligWerken
 {
-	/// <summary>
-	/// 
-	/// <para>Created by Mathias on 16-04-21</para>
-	/// </summary>
-	public class GameManager : Singleton<GameManager>
-	{
-		
-	}
+    /// <summary>
+    ///     <para>Created by Mathias on 12-05-2021</para>
+    /// </summary>
+    public class GameManager : Singleton<GameManager> { }
 }
---
+++ Assets/Veilig Werken/1. Scripts/Game/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using MBevers;
+using UnityEngine;
+
+namespace VeiligWerken
+{
+    /// <summary>
+    ///     This class handles the movement of the player and the accompanying animation.
+    ///     <para>Created by Mathias on 12-05-2021</para>
+    /// </summary>
+    public class PlayerMovement : ExtendedMonoBehaviour
+    {
+        private static readonly int ANIMATOR_HORIZONTAL = Animator.StringToHash("Horizontal");
+        private static readonly int ANIMATOR_VERTICAL = Animator.StringToHash("Vertical");
+        private static readonly int ANIMATOR_SPEED = Animator.StringToHash("Speed");
+
+        [SerializeField] private float movementSpeed = 10.0f;
+
+        private Animator animator;
+        private new Rigidbody2D rigidbody2D = null;
+        private Vector2 input = new Vector2();
+
+        private void Start()
+        {
+            rigidbody2D = ForceComponent<Rigidbody2D>();
+            animator = GetComponentIfInitialized<Animator>();
+        }
+
+        private void Update()
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            input.Normalize();
+
+            animator.SetFloat(ANIMATOR_HORIZONTAL, input.x);
+            animator.SetFloat(ANIMATOR_VERTICAL, input.y);
+            animator.SetFloat(ANIMATOR_SPEED, input.sqrMagnitude);
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 movement = input * (movementSpeed * Time.fixedDeltaTime);
+            rigidbody2D.MovePosition(rigidbody2D.position + movement);
+        }
+    }
+}
